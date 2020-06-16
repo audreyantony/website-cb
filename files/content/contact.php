ae666c969572b3267b2f9ebb72d0cae522f84846
audreyantony <web2020.audrey@gmail.com>
2020-06-16 14:32:11 +0200

    Page contact = CSS

---
+++ files/content/contact.php
@@ -0,0 +1,13 @@
+<section id="content">
+    <form>
+
+        <input id="name" type="text" placeholder="Votre nom">
+        <input id="email" type="text" placeholder="Votre e-mail">
+        <textarea id="message" placeholder="Votre message"></textarea>
+        <input id="send" type="submit" value="Envoyer">
+
+    </form>
+    <div class="boutonsimple">
+        <a href="?accueil"><button>Revenir à l'accueil</button></a>
+    </div>
+</section>
