2c81195b5a5267b60b4f07bb228900c29575cf1b
audreyantony <web2020.audrey@gmail.com>
2020-06-16 13:47:46 +0200

    Page a propos et contenu

---
+++ files/a-propos.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Tera&family=Montserrat:ital,wght@0,200;0,300;0,400;1,200;1,300;1,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Claude Buccella | À Propos</title>
+</head>
+<body>
+
+<!-- HEADER -->
+<?php
+require_once "content/header.php"
+?>
+
+<!-- CONTENU ACCUEIL -->
+<?php
+require_once "content/a-propos.php"
+?>
+
+<!-- FOOTER -->
+<?php
+require_once "content/footer.php"
+?>
+
+</body>
+</html>
---
+++ files/content/a-propos.php
@@ -0,0 +1,35 @@
+<div id="content">
+    <!-- CONTENU DE L'À PROPOS -->
+    <h1 id="titreapropos">À propos de l'auteur</h1>
+    <p id="apropos">Cette section est consacrée à la description et courte biographie de l’auteur. L’auteur, c’est moi, Claude. Enchanté de vous rencontrer !
+        <br><br><br>Depuis tout petit, j’ai principalement été élevé par ma télévision. J’ai appris à lire grâce aux comics de super-héros Marvel (les X-men, Spiderman, Daredevil, les Vengeurs et les 4 Fantastiques : merci ! –mention spéciale à Moon Knight et Deadpool, plus tard-) ; de la même façon, c’est aussi grâce à l’écriture de ces comics que j’ai appris à personnaliser tous mes personnages, grâce à leur niveau de langage ou leurs tics verbaux ; implanter une personnalité par la façon de parler, c’est une technique que tous les lecteurs ont expérimenté sans même s’en rendre compte ! Vous voulez une preuve ? Alors essayez d’imaginer le Capitaine Haddock dire « Ils sont fous, ces romains, par Toutatis ! », ou Lucky Luke lancer « bachibouzouks ! » et je ne vous parle pas de faire dire « M’enfin ?! » à un autre que Gaston Lagaffe (par contre, je verrais bien Panoramix expliquer « Une force surhumaine, la potion magique te donnera ! ». A cette époque, mon premier souvenir de récit écrit de ma main, est une rédaction scolaire relatant un combat de boxe diffusé la veille à la télévision, où le narrateur était le commentateur dudit match. Une page entière, totalement originale, qui a fait naître des sensations inconnues : la satisfaction d’avoir réussi à extraire de mon imagination, une idée claire et le plaisir de pouvoir m’y replonger aussi souvent que je le voulais, juste en la relisant. Un peu à la manière d’un Dumbledore qui extrait de son cerveau les souvenirs qu’il veut archiver dans son chaudron ; je pressais déjà mon imaginaire pour en sortir un nectar dans lequel je trempais ma plume pour de rapides poésies.
+        <br><br><br> Arrivé au collège, LE choc avait été très violent quand j’ai découvert un petit bouquin intitulé « Bilbo le Hobbit » ! Alors là, les amis, ouvrir ce livre a été l’une des meilleures idées de toute ma vie ! Sa couverture verte n’était pas engageante et pourtant, derrière cette maigre page cartonnée, tout un nouvel univers m’avait à la fois happé et envahi. Le monde des elfes, des guerres à coups d’épées contre les forces du mal et les dangers omniprésents dans les forêts denses, les montagnes creusées ou les eaux insondables, étaient désormais à moi et EN moi. Merci, John Ronald Reuel Tolkien ! <br><br><br>
+        C’était grâce à cette influence que mes poèmes de jeune adolescent perdu et torturé par une solitude accrue par ce que l’on désignera, vingt ans plus tard, comme une « phobie scolaire », étaient devenus de vraies histoires, plus longues, de plusieurs pages ; des nouvelles dont la première, que j’avais appelée « Le roman de Pégase », relatait l’espoir que j’avais de séduire la fille qui avait ravi mon petit cœur de l’époque, mêlant à tout cela des personnages, situations et lieux issus des différentes mythologies qui me passionnaient.
+        <br><br><br> Quelques années plus tard, avec l’accompagnement persistant de ma nourrice et meilleure amie : la télévision, j’avais décidé de ne plus me contenter de penser à ce qui pourrait améliorer ce que j’aimais dans mes séries préférées, mais de carrément les mettre en page. Sans jamais en avoir lu un, ni même avoir la moindre notion de la façon dont ils sont créés et mis en page, j’avais commencé à écrire, comme je le pouvais, mes premiers scénarios. Oui ; mes premiers. Car il n’avait pas fallu plus d’une poignée d’années pour que mon premier scénario (très nettement plus proche d’un roman que d’un scénario, pour tout avouer) soit rejoint par d’autres, nombreux, dans tous les genres et de toutes les durées. Et la rencontre d’un aspirant réalisateur m’avait alors donné l’impulsion ; jusque-là, personne n’avait jamais lu le moindre de mes écrits qui n’était pas un devoir scolaire. Lui, m’avait lu, apprécié et surtout, donné un nouvel objectif : lui écrire des scénarios pour avoir de quoi tourner des films !
+        <br><br><br> La formule a fonctionné et on ne s’est d’ailleurs, jamais vraiment quitté depuis notre rencontre, bien que presque tout nous opposait, à commencer par la géographie. J’ai pu entraîner et développer mon potentiel grâce à des idées et des objectifs que je n’aurais probablement jamais eu moi-même et cela m’a forcé à creuser en moi, à explorer autour de moi et à comprendre d’autres façons d’écrire et de faire passer des messages.
+        <br><br><br>
+        Depuis, j’ai eu l’occasion d’écrire, parallèlement à mes propres nouvelles ou romans et poèmes, des paroles de chansons, des clips, des pubs, des biographies (bien plus facile que d’écrire sa propre bio, même en version courte), des articles journalistiques, des chroniques d’humour, des thèses, des discours, des courts métrages, des longs métrages, des séries et même un livre pour enfant en exemplaire unique (étrangement satisfaisant, par ailleurs !).
+        <br><br><br>
+        Aujourd’hui, vous passez par ce site pour découvrir certains de mes écrits, pour me découvrir, moi-même, à travers eux et pour, je l’espère, rentrer dans mon univers que je pourrais comparer à une galaxie lointaine, très lointaine, parsemée de planètes toutes plus différentes les unes que les autres, où tout peut se passer, mais qui, pour ses visiteurs dépaysés et volontaires, n’est jamais hostile ou, je m’y efforce, sans intérêt.
+    </p>
+
+    <div class="boutonsimple">
+        <a href="?contact"><button>Contact</button></a>
+    </div>
+
+    <p>Je tiens à remercier, pour m’avoir donné du soutien, de la motivation, des envies, des perspectives, au-delà des créateurs de génie comme J.R.R. Tolkien ou Stan Lee,
+        <br>
+        Christian Estrada qui m’a fait découvrir Le Hobbit,<br>
+        José Branco qui m’a fait comprendre que je suis un auteur (et botté le cul, parfois),<br>
+        Damien Bonnel qui m’a ouvert des portes inconnues et inespérées,<br>
+        Audrey Antony qui croit en moi et en mes capacités plus que n’importe qui<br>
+
+        <br><br><br>
+        Chacun est la somme de ses choix, mais on fait ces choix grâce à ceux qui nous entourent.
+        <br><br><br>
+        Bon voyage !</p>
+
+    <div class="boutonsimple">
+        <a href="?accueil"><button>Revenir à l'accueil</button></a>
+    </div>
+</div>
